276016c17afad171d429364ddd4e6fa1e6aa8954
passos222 <148768070+passos222@users.noreply.github.com>
2024-07-31 07:52:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "você decidiu ir em uma pizzaria, você escolhe pizza de calabresa ou frango com catupiry?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmação"
+                texto: "calabresa!",
+                afirmação: "porra que bosta"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
+                texto: "frango com catupiry!",
+                afirmação: "tu tem bom gosto"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "você prefere levar ou comer no restaurante?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "afirmação"
+                texto: "levar.",
+                afirmação: "sem graça"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "afirmação"
+                texto: "comer no restaurante.",
+                afirmação: "otimo!!!"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "alguma bebida?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "afirmação"
+                texto: "sim!!.",
+                afirmação: "coisa linda"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "afirmação"
+                texto: "nâo.",
+                afirmação: "quem é o animal que come pizza no seco?"
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "vai querer adicionar borda de catupiry por apenas 9,99?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "afirmação"
+                texto: "não obrigado.",
+                afirmação: "quem te ensinou a comer pizza?"
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "afirmação"
+                texto: "sim por favor.",
+                afirmação: "bom gosto"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "qual  forma de pagamento? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "afirmação"
+                texto: "vai ser no pix.",
+                afirmação: "no pix tem 5% de desconto"
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "afirmação"
+                texto: "vai ser no dinheiro.",
+                afirmação: "dinheiro? que coisa do seculo passado"
             }
         ]
     },
@@ -77,13 +77,13 @@ let perguntaAtual;
 let historiaFinal = ""; 
 
 function mostraPergunta() {
- /*    if(atual >= perguntas.length){
+    if(atual >= perguntas.length){
         mostraResultado();
         return;
     }   */ 
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
- /* caixaAlternativas.textContent = ""; */
+  caixaAlternativas.textContent = ""; 
     mostraAlternativas();
 }
 function mostraAlternativas() {
@@ -96,16 +96,16 @@ function mostraAlternativas() {
 }
 
 function respostaSelecionada(opcaoSelecionada){
-            const afirmacoes = opcaoSelecionada.afirmacoes; /* mudar para opcaoSelecionada.afirmacao*/
-            historiaFinal = afirmacoes; /* += afirmacoes + " "; */
+            const afirmações = opcaoSelecionada.afirmação;
+            historiaFinal += afirmações + " ";
             atual++;
             mostraPergunta();
       }
-/*
+
 function mostraResultado(){
     caixaPerguntas.textContent = "Em 2049..."
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent ="";
 }
-*/
+
 mostraPergunta();
